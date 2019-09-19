5bb30f64f1522f24c935d7aaf9c8175366f83cfe
Matthew Vorisek <matthew.p.vorisek@nasa.gov>
2019-09-19 16:34:35 -0400

    NavBar Updates

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Alignment, Button } from '@blueprintjs/core';
+import { Navbar, Alignment, Button, Popover, Menu, Position, Callout } from '@blueprintjs/core';
 
 import { toggleTheme } from '../../redux/slices/ThemeSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { POSITION_BOTTOM } from '@blueprintjs/core/lib/esm/common/classes';
+import { Box } from 'grommet';
 
 function NavBar() {
   const dispatch = useDispatch();
@@ -11,22 +13,39 @@ function NavBar() {
 
   function ThemeToggle() {
     return (
-      <Button icon={theme === 'light' ? 'moon' : 'flash'} minimal large onClick={() => dispatch(toggleTheme())}/>
+      <Menu.Item icon={theme === 'light' ? 'moon' : 'flash'} minimal large onClick={() => setTimeout(() => { dispatch(toggleTheme()) }, 50)} text={theme === 'light' ? 'Dark Theme' : 'Light Theme'}/>
     );
   }
 
   return (
     <Navbar>
-      <Navbar.Group align={Alignment.LEFT}>
-        <Navbar.Heading>Test App</Navbar.Heading>
-        <Navbar.Divider />
-        <Link className="navButton" to="/"><Button minimal icon="home" text="Home"></Button></Link>
-        <Link className="navButton" to="/about/"><Button minimal icon="info-sign" text="About"></Button></Link>
-        <Link className="navButton" to="/users/"><Button minimal icon="people" text="Users"></Button></Link>
-      </Navbar.Group>
-      <Navbar.Group align={Alignment.RIGHT}>
-        <ThemeToggle />
-      </Navbar.Group>
+      <Box direction="row" justify="between" >
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Test App</Navbar.Heading>
+          <Navbar.Divider />
+          <Link className="navButton" to="/"><Button minimal icon="home" text="Home"></Button></Link>
+          <Link className="navButton" to="/about/"><Button minimal icon="info-sign" text="About"></Button></Link>
+          <Link className="navButton" to="/users/"><Button minimal icon="people" text="Users"></Button></Link>
+        </Navbar.Group>
+        <Navbar.Group>
+          <Callout intent="primary">
+            New Info available! Click here for more information!
+          </Callout>
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <div className="bp3-text-large">Welcome, <b>John Doe</b>!</div>
+          <Navbar.Divider />
+          <Popover content={
+            <Menu>
+              <ThemeToggle />
+              <Menu.Divider />
+              <Menu.Item icon="log-out" intent="danger" text="Logout" />
+            </Menu>
+          } position={Position.BOTTOM_RIGHT} minimal large>
+              <Button minimal rightIcon="chevron-down"/>
+          </Popover>
+        </Navbar.Group>
+      </Box>
     </Navbar>
   );
 }
